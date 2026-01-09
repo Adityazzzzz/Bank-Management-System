1104f4172551877099813ba81845fc6103b5fa16
Aditya Singh <adityasinghrajawat2004@gmail.com>
2026-01-09 16:03:10 +0530

    category fixed

--- citadel/components/RightSidebar.tsx
+++ citadel/components/RightSidebar.tsx
@@ -4,6 +4,7 @@ import React from 'react'
 import BankCard from './BankCard'
 import { countTransactionCategories } from '@/lib/utils'
 import Category from './Category'
+import PlaidLink from './PlaidLink' 
 
 const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
   const categories: CategoryCount[] = countTransactionCategories(transactions);
@@ -29,19 +30,10 @@ const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
       </section>
 
       <section className="banks">
-        <div className="flex w-full justify-between">
+        <div className="flex w-full justify-between items-center">
           <h2 className="header-2">My Banks</h2>
-          <Link href="/" className="flex gap-2">
-            <Image 
-               src="/icons/plus.svg"
-              width={20}
-              height={20}
-              alt="plus"
-            />
-            <h2 className="text-14 font-semibold text-gray-600">
-              Add Bank
-            </h2>
-          </Link>
+          
+          <PlaidLink user={user} variant="ghost" />
         </div>
 
         {banks?.length > 0 && (
@@ -72,7 +64,7 @@ const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
 
           <div className='space-y-5'>
             {categories.map((category, index) => (
-              <Category key={category.name} category={category} />
+              <Category key={category.name} title={category.name} />
             ))}
           </div>
         </div>
